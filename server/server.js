95c3167e5c80e366a0cf55981504550600c4c02c
Andi Conner <andiconner@icloud.com>
2022-03-08 22:18:10 -0500

    mutations

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -13,13 +15,13 @@ const startServer = async () => {
   // create a new Apollo server and pass in our schema data
   const server = new ApolloServer({ 
     typeDefs, 
-    resolvers, 
+    resolvers,
+    context: authMiddleware
      
   });
 
   // Start the Apollo server
   await server.start();
-
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
 
