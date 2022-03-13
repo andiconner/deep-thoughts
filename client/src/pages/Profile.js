bd2629c967220e72ea2968c227ecc229579937c7
Andi Conner <andiconner@icloud.com>
2022-03-13 12:04:45 -0400

    friend mutation added

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,15 +4,18 @@ import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 
 const Profile = () => {
-  const { username: userParam } = useParams();
+  const { username: userParam } = useParams()
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam } //he useParams Hook retrieves the username from the URL, which is then passed to the useQuery Hook
   });
+
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   const user = data?.me || data?.user || {};
 // redirect to personal profile page if username is the logged-in user's
@@ -31,12 +34,28 @@ if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <div className="flex-row mb-3">
-      <h2 className="bg-dark text-secondary p-3 display-inline-block">
-        Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-      </h2>
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+        {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
